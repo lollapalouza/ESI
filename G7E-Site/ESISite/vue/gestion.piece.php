580b46709539b511fe660f496a4eea33bed381a9
Mathieu <mathieu.tran2901@outlook.com>
2019-05-16 01:34:20 +0200

    update

--- G7E-Site/ESISite/vue/gestion.piece.php
+++ G7E-Site/ESISite/vue/gestion.piece.php
@@ -113,10 +113,10 @@ if(isset($_POST['formsuppression'])){
                      z = 255;
                 }
 
-                newLI.style.width =  Math.random()*100 + 100 + "px";
+                newLI.style.width =  Math.round(Math.random()*250 + 100) + "px";
                 newLI.style.color = "rgb(" + x + "," + y + "," + z + ")";
                 newLI.style.fontFamily = "Segoe UI";
-                newLI.style.fontSize = 25 + "px";
+                newLI.style.fontSize = 20 + "px";
                 newLI.style.background = "rgb(" + xcolor + "," + ycolor + "," + zcolor + ")";
                 newLI.style.cursor = "pointer";
 
