631ffd2adc4ba877ecea9e6ff289dc1d848b91f7
Alan Mithouard <alan.mithouard@gmail.com>
2019-05-17 15:52:19 +0200

    COMMIT 17/05 2 Alan
    Page accueil hors connexion

--- G7E-Site/ESISite/vue/AccueilHorsConnexion.php
+++ G7E-Site/ESISite/vue/AccueilHorsConnexion.php
@@ -25,6 +25,25 @@
     <div class="image_ESI" align="center"><img id="Logo" src="vue/image/logoESI.png" alt="icone logo"/></div>
 </div>
 
+<div class="conteneur3">
+    <br /><br />
+    <h2><strong> ESI, la maison faite pour vous !</strong></h2>
+    <br /><br />
+    <div class="textpresentation">
+    ESI, c'est un site codé pour un groupe de six ingénieurs qui ont pour but de révolution le monde de demain. Après plus d'un an de développement,
+    l'entreprise ANVI, on est fière de vous présenter son projet de domotique commandé par l'entreprise DOMISEP.
+    <br/>
+        L'offre que nous proposons est diverse et variée car elle comporte l'implémentation de capteurs de température, présence et luminosité. Ses capteurs sont couplés
+        à des actionneurs afin de que votre maison soit totalement autonome. En effet, plus besoin d'ouvrir vos fenêtres, d'allumer votre chauffage ou fermer les volets :
+        <br/>
+    <h3>
+    ESI s'occupe de tout
+
+    </h3>
+    </div>
+
+</div>
+<br /><br /><br />
 <div class="conteneur">
     <br /><br />
     <h2><strong> Une offre adaptée à vos besoins !</strong></h2>
@@ -34,37 +53,55 @@
     </div>
     <div class="pack2">
         <div class="pack2-1">
-            <h3a>-  Une surveillance automatique de votre habitat 24/7 </h3a>
+            <h3a> - Vos proches en sécurité</h3a>
             <br /><br />
-            <h3a> - Lorem Ipsum</h3a>
+            <h3a> - Intervention rapide</h3a>
             <br /><br />
-            <h3a> - Lorem Ipsum</h3a>
+            <h3a>- Service 7/7 24/24 </h3a>
+            <br /><br />
+            <h3a> - Prise en charge complète </h3a>
+        </div>
+        <div class="pack2-1">
+            <h3a>-  Alerte instantanée en cas d'intrusion </h3a>
+            <br /><br />
+            <h3a> - Prévention des forces de l'ordre</h3a>
+            <br /><br />
+            <h3a> - Service 7/7 24/24</h3a>
+            <br /><br />
+            <h3a> - Sécurisation du domaine</h3a>
         </div>
         <div class="pack2-1">
             <h3a>-  Une surveillance automatique de votre habitat 24/7 </h3a>
             <br /><br />
-            <h3a> - Lorem Ipsum</h3a>
+            <h3a> - Caméra activée en cas d'intrusion</h3a>
             <br /><br />
-            <h3a> - Lorem Ipsum</h3a>
-        </div>
-        <div class="pack2-1">
-            <h3a>-  Une surveillance automatique de votre habitat 24/7 </h3a>
+            <h3a> - Alerte instantanée sur votre appareil</h3a>
             <br /><br />
-            <h3a> - Lorem Ipsum</h3a>
-            <br /><br />
-            <h3a> - Lorem Ipsum</h3a>
+            <h3a> - Enregistrement dans la base de données</h3a>
         </div>
     </div>
 
 </div>
-<div class="conteneur2">
-</div>
-<div class="conteneur">
-</div>
-<div class="conteneur2">
+
+<div class="conteneur3">
+    <br /><br />
+    <h2><strong> ESI, un gage de qualité!</strong></h2>
+    <br /><br />
+    <div class="textpresentation">
+        ESI, c'est un site codé par des ingénieurs experts en programmation, électronique, traitement du signal et télécom. Ce site est administré par
+        l'entreprise DOMISEP qui est connue pour son sérieux et sa longévité dans ce domaine.
+        <br/>
+        Vérifié et approuvé par de nombreux professionnels, , ce site respecte la protection des données de ces client. La sécurité est aussi un point qui est au centre
+        des problématiques de DOMISEP.
+        <br/>
+        <h3>
+            ESI, c'est 99% de satisfaction client
+
+        </h3>
+    </div>
+
 </div>
 
-<br /><br /><br /><
 
 
 </body>
