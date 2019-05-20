100a6b8833ca46820b84fa45ee88633c351f03b0
Mathieu <mathieu.tran2901@outlook.com>
2019-05-20 12:00:51 +0200

    capteur

--- G7E-Site/ESISite/modeles/requete.capteur.php
+++ G7E-Site/ESISite/modeles/requete.capteur.php
@@ -7,5 +7,31 @@
  */
 include('requete.generique.php');
 
+function affiche_capteur_actionneur(PDO $bdd, $idpiece){
+    $query=$bdd->prepare('SELECT * FROM catalogue WHERE idPiece = :idpiece');
+    $query->bindValue('idpiece', $idpiece, PDO::PARAM_STR);
+    $query->execute();
+    return $fin=$query->fetchall();
+}
 
+function capteur_actionneur_supprimer(PDO $bdd, $idpiece, $nom){
+    $query=$bdd->prepare('DELETE FROM catalogue WHERE idPiece = :id AND Nom = :nom');
+    $query->bindValue(':id',$idpiece, PDO::PARAM_STR);
+    $query->bindValue(':nom',$nom, PDO::PARAM_STR);
+    $query->execute();
+    return $fin=$query->fetch();
+}
+
+function inserer_capteur_actionneur(PDO $bdd, $nom, $type, $serie, $idCategorie, $idPiece)
+{
+    $inserer = $bdd->prepare("INSERT INTO catalogue(Nom, Type_, Numero_de_serie, idCategorieProduit, idPiece) VALUES (?,?,?,?,?)");
+    return $inserer->execute(array($nom, $type, $serie, $idCategorie, $idPiece));
+}
+
+function rechercher_capteur_actionneur(PDO $bdd, $valeur){
+    $query=$bdd->prepare('SELECT ID FROM categorie_produit WHERE Nom = :nom');
+    $query->bindValue(':nom',$valeur, PDO::PARAM_STR);
+    $query->execute();
+    return $fin=$query->fetch();
+}
 ?>
--- G7E-Site/ESISite/vue/gestion.capteur.php
+++ G7E-Site/ESISite/vue/gestion.capteur.php
@@ -3,6 +3,41 @@
  * Page pour AJAX de gestion.capteur
  */
 include('modeles/requete.capteur.php');
+
+
+$idpiece = $_SESSION['piece'];
+
+if(isset($_POST['form'])){
+    $nom = htmlspecialchars($_POST['formNom']);
+    $type = htmlspecialchars($_POST['type']);
+    $choix = htmlspecialchars($_POST['add']);
+    $serie = htmlspecialchars($_POST['formSerie']);
+    $data = rechercher_capteur_actionneur($bdd,$choix);
+    if(!empty($_POST['type']) AND !empty($_POST['formSerie'])){
+        inserer_capteur_actionneur($bdd, $nom, $type, $serie, $data['ID'], $idpiece);
+        $erreur = "success";
+    }
+    else{
+        $erreur = "remplir les champs";
+    }
+}
+
+if(isset($_POST['formSuppression'])){
+    $suppression = htmlspecialchars($_POST['formSuppression1']);
+    $supression_confirmation = htmlspecialchars($_POST['formSuppression2']);
+    if(!empty($_POST['formSuppression1']) AND !empty($_POST['formSuppression2'])){
+        if ($suppression == $supression_confirmation){
+            capteur_actionneur_supprimer($bdd, $idpiece, $suppression);
+            $erreur2 = "success";
+        }
+        else{
+            $erreur2 = "Les noms ne correspondent pas";
+        }
+    }
+    else{
+        $erreur2 = "remplir les champs";
+    }
+}
 ?>
 
 
@@ -43,46 +78,136 @@ include('modeles/requete.capteur.php');
     <div class="conteneur_haut">
         <ul id="capteurs">
         </ul>
+        <?php
+        $occ = 0;
+        $tabid = array();
+        $tabnom = array();
+        $data2 = affiche_capteur_actionneur($bdd, $idpiece);
+        foreach($data2 as $value){
+            $tabid[] = $value["ID"];
+            $tabnom[] = $value["Nom"];
+        }
+        $tab_to_json_id = json_encode((array)$tabid);
+        $tab_to_json_nom = json_encode((array)$tabnom);
+        ?>
+
+        <script type="text/javascript">
+            var tabid_php = <?php echo $tab_to_json_id ?>;
+            var tabnom_php = <?php echo $tab_to_json_nom ?>;
+
+            for (var i=0; i< tabid_php.length; i++){
+                var newLI = document.createElement("LI");
+                var nom = tabnom_php[i];
+                newLI.style.fontFamily = "Segoe UI";
+                newLI.style.fontSize = 20 + "px";
+                newLI.style.cursor = "pointer";
+                newLI.className = "liste";
+                newLI.id = tabid_php[i];
+                newLI.innerHTML = nom;
+                /*newLI.addEventListener("click", function () {
+                    var id_session = this.id;
+                    $.post("vue/Ajax/piece.php",{id_sess: id_session},function(){
+                        document.location.href="index.php?cible=capteur&fonction=capteur"
+                    })
+                });
+                */
+                document.getElementById('capteurs').appendChild(newLI);
+            }
+        </script>
     </div>
     <div class="conteneur_bas">
-        <form method="POST" action="" align ="center">
-            <table>
-                <tr>
-                    <td>
-                        <input name="formNom" type="text" placeholder="         nom du capteur" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <select class="select" name="type" id="type" onchange="comparaison()">
-                            <option value="" id="cacher">Selectionner</option>
-                            <option value="capteur">Capteur</option>
-                            <option value="actionneur">Actionneur</option>
-                        </select>
-                        <script language="JavaScript">
-                            document.getElementById('cacher').style.fontWeight = 'bold';
-                            document.getElementById('type').style.fontWeight = 'bold';
-                        </script>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <select class="select" name="add" id="add">
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <input name="formSerie" type="text" placeholder="            N° de série" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <input name="form" type="submit"/>
-                    </td>
-                </tr>
-            </table>
-        </form>
+        <div class="ajouter">
+            <form method="POST" action="" align ="center">
+                <table>
+                    <tr>
+                        <td>
+                            <div class = "ecriture">AJOUTER</div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input name="formNom" type="text" placeholder="         nom du capteur" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <select class="select" name="type" id="type" onchange="comparaison()">
+                                <option value="" id="cacher">Selectionner</option>
+                                <option value="capteur">Capteur</option>
+                                <option value="actionneur">Actionneur</option>
+                            </select>
+                            <script language="JavaScript">
+                                document.getElementById('cacher').style.fontWeight = 'bold';
+                                document.getElementById('type').style.fontWeight = 'bold';
+                            </script>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <select class="select" name="add" id="add">
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input name="formSerie" type="text" placeholder="            N° de série" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input name="form" type="submit"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <?php
+                            if(isset($erreur))
+                            {
+                                echo '<font size="2px" color="white">'. '<strong>' . $erreur."</font>" . '</strong>';
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+        <div class="ajouter">
+            <form method="POST" action="" align ="center">
+                <table>
+                    <tr>
+                        <td>
+                            <div class="ecriture">SUPPRIMER</div>
+                            <br/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input name="formSuppression1" type="text" placeholder="            Supprimer" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input name="formSuppression2" type="text" placeholder="            Confirmation" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input name="formSuppression" type="submit"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <?php
+                            if(isset($erreur2))
+                            {
+                                echo '<font size="2px" color="white">'. '<strong>' . $erreur2."</font>" . '</strong>';
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
     </div>
 </div>
 </body>
