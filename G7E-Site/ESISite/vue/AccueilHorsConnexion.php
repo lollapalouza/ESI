495c6465d49d1ad5a2bbeab826a7575ff1287936
Mathieu <mathieu.tran2901@outlook.com>
2019-05-27 20:41:39 +0200

    video

--- G7E-Site/ESISite/vue/AccueilHorsConnexion.php
+++ G7E-Site/ESISite/vue/AccueilHorsConnexion.php
@@ -5,10 +5,9 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="vue/CSS/css.accueilHorsConnexion.css"/>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://icono-49d6.kxcdn.com/icono.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+
+
+
 
     <!-- Titre de la page -->
 
@@ -21,25 +20,22 @@
 <div class="conteneur_total">
 
     <div id="conteneur_header"> <!-- Le conteneur -->
-        <div class="HeaderPartieGauche" >  <!-- Le logo des reseaux sociaux -->
+        <div class="HeaderPartieGauche" id="logo">  <!-- Le logo des reseaux sociaux -->
             <a href="index.php?cible=utilisateur&fonction=AccueilHorsConnexion"><img src="vue/image/logoESI.png" alt="ESI"/></a>
         </div>
-        <div class="HeaderPartieDroit"> <!-- Lien pour nous contacter -->
+        <div class="HeaderPartieDroit" id="lien"> <!-- Lien pour nous contacter -->
             <a class="inscription" href = "index.php?cible=utilisateur&fonction=inscription">Inscription</a>
             <a class="connexion" href = "index.php?cible=utilisateur&fonction=pageconnexion">Connexion</a>
         </div>
-        <div class="BarreHeader"><p></p></div>
     </div>
 
-    <div class="presentation">
-
-        <h1> Révolutionnez votre quotidien </h1>
-        <br/>
-        <h2> et entrez dans une <em><strong> nouvelle dimension</strong></em></h2>
-        <br/>
-        <h3> avec </h3>
-        <div class="image_ESI" align="center"><img id="Logo" src="vue/image/logoESI.png" alt="icone logo"/></div>
-    </div>
+    <script>
+        apparition = function(id){
+            setTimeout(function() {var elmt = document.getElementById(id); elmt.style.opacity = 1;}, 2000);
+        };
+        onload = apparition("logo");
+        onload = apparition("lien")
+    </script>
 
     <div class="video_container">
         <video autoplay loop>
